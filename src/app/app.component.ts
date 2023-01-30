d047530ae9e40778a4ff6c8391489cd8c1b8c933
Adolfo <adolfo.pereira@sap.com>
2023-01-29 23:26:16 -0500

    Modifying Diagram

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bpmn-js-angular';
-  diagramUrl = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
-  importError?: Error;
+    title = 'bpmn-js-angular'
+    // diagramUrl = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+    diagramUrl = 'https://bpmn.io/assets/attachments/blog/2016/colors.bpmn'
+    importError?: Error
 
-  handleImported(event: any) {
+    handleImported(event: any) {
+        const { type, error, warnings } = event
 
-    const {
-      type,
-      error,
-      warnings
-    } = event;
+        if (type === 'success') {
+            console.log(`Rendered diagram (%s warnings)`, warnings.length)
+        }
 
-    if (type === 'success') {
-      console.log(`Rendered diagram (%s warnings)`, warnings.length);
+        if (type === 'error') {
+            console.error('Failed to render diagram', error)
+        }
+
+        this.importError = error
     }
-
-    if (type === 'error') {
-      console.error('Failed to render diagram', error);
-    }
-
-    this.importError = error;
-  }
-
 }
