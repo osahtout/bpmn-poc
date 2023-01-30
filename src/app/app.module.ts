781f9fdd0303852d3670faa9f65603b3107f7a1d
Adolfo <adolfo.pereira@sap.com>
2023-01-30 00:04:45 -0500

    Importing missing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,20 @@ import { DiagramComponent } from './diagram/diagram.component'
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormModule } from '@fundamental-ngx/core/form'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
     declarations: [AppComponent, DiagramComponent],
     providers: [],
     bootstrap: [AppComponent],
-    imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, HttpClientModule, FormModule],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
 })
 export class AppModule {}
